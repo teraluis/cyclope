b432b254924a40e81843d45b9d26ea732ea20e67
Luis Manresa <luis.manresa@fabrick-io.com>
2021-03-28 23:07:21 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {WelcomeComponent} from './modules/welcome/welcome.component';
+import {QuestionComponent} from './modules/question/question.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent, children: [
+      {path: '', pathMatch: 'full', redirectTo: 'welcome'},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: 'questions', component: QuestionComponent},
+      {path: 'game-over', component: WelcomeComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
