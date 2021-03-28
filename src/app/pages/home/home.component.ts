2ce21548f568d895dc9209355ff0ee604aa4592f
Luis Manresa <luis.manresa@fabrick-io.com>
2021-03-28 23:41:36 +0200

    welcome

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  name = 'cyclope';
+  time = 60;
   constructor() { }
 
   ngOnInit(): void {
